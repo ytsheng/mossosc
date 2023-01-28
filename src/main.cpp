a53c8bc80264d0f95a922a9eb5a37922741d4dcc
Sasha <sashashengyt@gmail.com>
2023-01-27 21:06:38 -0800

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ Imu imu;
 OneButton button(9);
 WiFiUDP Udp;
 unsigned int localPort = 8888;
-char ssid[] = "Bears don't dig on dancing"; // your network SSID (name)
-char pass[] = "dancedance44"; // your network password
+char ssid[] = "ButernutMansion"; // your network SSID (name)
+char pass[] = "yellowsubmarine69"; // your network password
 
 void setup()
 {
@@ -40,24 +40,25 @@ void loop()
   static uint32_t last_print = 0;
   if (millis() - last_print > 500) {
     Serial.printf("Roll: %f  Pitch: %f  Yaw: %f\r\n", imu.getRoll(), imu.getPitch(), imu.getYaw());
-    OSCMessage msg("/roll");
+    // OSCMessage msg("/roll");
+    // msg.add(map(imu.getRoll(), -180, 180, 0, 1));
+    // Udp.beginPacket("192.168.1.218", 9000);
+    // msg.send(Udp);
+    // Udp.endPacket();
 
-    msg.add(imu.getRoll());
-    Udp.beginPacket("192.168.1.234", 9000);
-    msg.send(Udp);
-    Udp.endPacket();
-
-    OSCMessage msg2("/pitch");
-    msg2.add(imu.getPitch());
-    Udp.beginPacket("192.168.1.234", 9000);
+    // OSCMessage msg2("/pitch");
+    OSCMessage msg2("/lx/mixer/channel/4/pattern/4/fuel");
+    // Serial.println(map(imu.getPitch(), -90, 90, 0, 1));
+    msg2.add((imu.getPitch() + 100)/200);
+    Udp.beginPacket("192.168.1.244", 3030);
     msg2.send(Udp);
     Udp.endPacket();
 
-    OSCMessage msg3("/yaw");
-    msg3.add(imu.getYaw());
-    Udp.beginPacket("192.168.1.234", 9000);
-    msg3.send(Udp);
-    Udp.endPacket();
+    // OSCMessage msg3("/yaw");
+    // msg3.add(map(imu.getYaw(), 0, 360, 0, 1));
+    // Udp.beginPacket("192.168.1.218", 9000);
+    // msg3.send(Udp);
+    // Udp.endPacket();
 
     last_print = millis();
   }
